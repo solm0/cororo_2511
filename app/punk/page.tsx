3bc812ad386ba4511df7b2c60f55e992ba8c0802
armajk2 <145760299+armajk2@users.noreply.github.com>
2025-11-06 05:14:56 +0900

    z

---
+++ app/punk/page.tsx
@@ -0,0 +1,12 @@
+export default function Page() {
+  return (
+    <video
+      src="/static/Punk.mp4"
+      className="w-screen h-screen object-cover"
+      autoPlay
+      loop
+      muted
+      playsInline
+    />
+  )
+}
