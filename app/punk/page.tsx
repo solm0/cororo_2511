6e9d04767aed91334a6203fe8aaf85ce8feabdf1
armajk2 <145760299+armajk2@users.noreply.github.com>
2025-11-06 07:48:09 +0900

    ㅌ

--- app/punk/page.tsx
+++ app/punk/page.tsx
@@ -1,8 +1,15 @@
 export default function Page() {
   return (
-<video controls className="w-screen h-screen">
-        <source src={'/static/punk/punk.mp4'} type={`video/mp4`} />
-        Your browser does not support the video tag.
- </video>
+    <video
+      controls
+      autoPlay
+      loop
+      muted
+      playsInline
+      className="w-screen h-screen object-cover"
+    >
+      <source src="/static/punk/punk.mp4" type="video/mp4" />
+      Your browser does not support the video tag.
+    </video>
   )
 }
