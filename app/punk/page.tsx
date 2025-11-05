703f2b41de78bf203418efef305361d37c881386
armajk2 <145760299+armajk2@users.noreply.github.com>
2025-11-06 05:22:24 +0900

    c

--- app/punk/page.tsx
+++ app/punk/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
   return (
     <video
-      src="/static/Punk.mp4"
+      src="/public/static/Punk.mp4"
       className="w-screen h-screen object-cover"
       autoPlay
       loop
